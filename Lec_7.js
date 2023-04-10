72cb29ab10d9caf3f72660bca50a00fd6c82fe61
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-04-10 22:19:18 +0530

    Display current date and time

---
+++ Lec_7.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name="Rajeev";
+const currDate=new Date().toLocaleDateString();
+const currTime=new Date().toLocaleTimeString();
+
+ReactDOM.render(
+  <>
+    <h1>Hello, My name is {name}</h1>
+    <p>Current Date is = {currDate}</p>
+    <p>Current Time is = {currTime}</p>
+  </>,
+  document.getElementById("root")
+)
