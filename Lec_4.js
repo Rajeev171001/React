f8371198741af11798fe210168e6e4540b23d8a4
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-04-10 21:56:15 +0530

    Netflix Movie List in Ordered List

---
+++ Lec_4.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <h1>Netflix Movie List in Oredred List</h1>
+    <p>Here the list of my 5 netflix series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra curricular</li>
+      <li>My Holo Love</li>
+      <li>My first Love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+)
