c768b0269b804d7e8a0fb24a6c75c526ab3e1a54
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-04-17 22:53:08 +0530

    Lec_10

---
+++ App.jsx
@@ -0,0 +1,11 @@
+const youtuber="Rajeev Sharma";
+const favprog="Ract js";
+
+function myName(){
+    let name="RAjeev"
+    return name;
+}   
+
+export default youtuber;  // only one can be deafult
+
+export {favprog, myName};
