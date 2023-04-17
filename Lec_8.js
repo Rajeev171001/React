156015cf299d12b16ea9a3f3cd2e85bbfb568011
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-04-17 22:10:03 +0530

    components

---
+++ Lec_8.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from "./Heading";
+import Para from "./para";
+
+ReactDOM.render(
+  <>
+    
+    <Heading /> 
+    <Para />
+    <ol>
+      <li>Rajeev</li>
+      <li>Sharma</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
