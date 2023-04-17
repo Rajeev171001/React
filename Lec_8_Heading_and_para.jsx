f7c3a0c602b18ce3c53b8295a52b7cc4423f843a
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-04-17 22:11:24 +0530

    Heading and para

---
+++ Lec_8_Heading_and_para.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Heading(){
+    return <h1>This is Rajeev</h1>
+}
+
+export default Heading;
+
+
+
+
+
+
+import React from "react";
+
+function para(){
+    return <p>My name is Rajeev</p>
+}
+
+export default para;
