c414f3ba6d3b96593275c37f8db255d0ee31ba2b
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-08-19 14:49:25 +0530

    Update Lec_9.js

--- Lec_9.js
+++ Lec_9.js
@@ -1,15 +1,16 @@
 import react from "react";
 import ReactDOM from "react-dom";
 import myfav, {favprog, myName} from "./App";
+import * as ques from "./App";  // jab ek sath saare import krne ho
 
 
 ReactDOM.render(
   <>
     <ol>
-      <li>Rajeev</li>
-      <li>{myfav}</li>
-      <li>{favprog}</li>
-      <li>{myName()}</li>
+      <li>Rajeev</li>  
+      <li>{myfav}</li>   //<li>{ques.myfav}</li> 
+      <li>{favprog}</li>  //<li>{ques.favprog}</li>
+      <li>{myName()}</li>  //<li>{ques.myName()}</li>
     </ol>
   </>,
   document.getElementById("root")
